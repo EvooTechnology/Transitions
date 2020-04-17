c316396e6ddc5c140dcb2078d756f5bf4ff4c096
Francisco Rodríguez <francisco_rc98@hotmail.com>
2020-04-17 10:36:05 +0200

    Added Tutorial and more ready to use transitions.
    Optimize some animations.
    
    Added: LoadingScreen prefab.
    Added: Tutorial.

---
+++ Scripts/AutoRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    public Vector3 rotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.localEulerAngles = gameObject.transform.localEulerAngles + rotate;
+    }
+}
